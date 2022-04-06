c09a950feeeb983a4ed5e0e4adc322d062eb6411
Emmanuele Macedo <emmanuele.pampolha@al.infnet.edu.br>
2022-04-06 00:54:40 -0300

    ainda tentando resolver as questões do AT

--- Questao5_letraB_AT.py
+++ Questao5_letraB_AT.py
@@ -1,22 +1,33 @@
-import csv
+listaPib = [
+        ["EUA", 16.76, 22.48],
+        ["China", 9.46, 16.15],
+        ["Japão", 4.92, 4.93],
+        ["Alemanha", 3.73, 4.1],
+        ["Reino Unido", 2.68, 3.73],
+        ["França", 2.8, 3.01],
+        ["Brasil", 2.39, 2.35],
+        ["Itália", 2.13, 2.17],
+        ["Índia", 1.87, 3.64],
+        ["Rússia", 2.07, 2.08],
+        ["Canadá", 1.83, 2.04],
+        ["Coreia do Sul", 1.3, 2.01],
+        ["Espanha", 1.39, 1.48],
+        ["México", 1.26, 1.65],
+        ["Indonésia", 9.13, 1.3]
+]
+nomePais = [0]
 
-variacao = 0
-variacaoInicial = 0
-variacaoFinal = 0
+for contador in range(len(listaPib)):
+    variacao = 0
+    variacaoInicial = 0
+    variacaoFinal = 0
 
-try:
-    with open('lista_PIB.csv', mode="r", encoding="utf8") as arquivo:
-        dados = csv.reader(arquivo, delimiter=";")
+    def calculaVariacao(indicePib):
+        variacaoInicial = float(indicePib[1])
+        variacaoFinal = float(indicePib[2])
 
-        listaPib = list(dados)
+        variacao = (((variacaoFinal / variacaoInicial) - 1) * 100)
 
-        linha = 0
-        for dado in listaPib:
-            variacao = ((variacaoFinal / variacaoInicial) - 1) * 100
+        return variacao
 
-            print(f"{dado}\t\t Variação de {variacao} entre 2013 e 2020.")
-            coluna = coluna + 1
-        linha = linha + 1
-
-finally:
-    print("Fim")
+    print(f"{nomePais[0]}\t\t Variação de {variacao} entre 2013 e 2020.")
